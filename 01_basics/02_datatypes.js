ef07a65edecf6f2480af7b001e22db2a7c41f190
varunvantala <152497039+varunvantala@users.noreply.github.com>
2023-12-04 11:29:19 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+// "use strict";//treat all js code as newer version
+//alert(3+3)//we are using nodejs , not browser
+// console.log(3
+//     +
+//     3)//code readibility should be high
+  console.log("hitesh")  
+  let age = 18
+  let isloggedin = false
+  let state;//undefined
+  let state1 = null//intentionally we are saying there is no value
+
+  //number=> 2 to power 53
+  //bigint
+  //string=" "
+  //boolean = true/false
+  //null = standalone value
+  //undefined = 
+  //symbol = unique
+  //object
+  console.log(typeof undefined);//undefined
+  console.log(typeof null);//object
+
