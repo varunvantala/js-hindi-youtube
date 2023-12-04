730f901b70ab70ccd0d22921a76468fd167b41c0
varunvantala <152497039+varunvantala@users.noreply.github.com>
2023-12-04 11:31:12 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "varun@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+//accountId = 2//here it is not allowed to change because we have declare with const variable
+accountEmail = "hchc@gmail.com"
+accountPassword ="221112"
+accountCity = " bengaluru"
+//here prefer not to use var only use let
+console.table([accountId,accountEmail,accountCity,accountPassword,accountState])
