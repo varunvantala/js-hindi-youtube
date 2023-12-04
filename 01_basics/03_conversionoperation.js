d81b46ab9e2e75f26d1ea96f329f8bf65f9bdf7f
varunvantala <152497039+varunvantala@users.noreply.github.com>
2023-12-04 13:12:41 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,20 @@
+// let score="33"
+// console.log(typeof (score))
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+//"33"=>33 type is number
+//"33abc"=>NaN(not a number ) but type is number
+//true=>1 , false=>0
+// let isLoggedIn=1
+// let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn);
+//1=>true
+//0=>false
+//" varun">=true
+//" ">=false
+let someNumber=33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
